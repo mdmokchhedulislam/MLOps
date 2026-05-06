c82ac6f18af587dc3f09a7e885714b08e31d98fa
mokchhedul.islam@spectrum-bd.com <mokchheduls46@gmail.com>
2026-05-06 09:57:16 +0600

    simple ml project with pandas and linearRegression

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,62 @@
+####### simple practice with linearRegression
+
+# from sklearn.linear_model import LinearRegression
+
+# X=[[10], [20], 30]
+# y=[100,200,300]
+
+
+# model = LinearRegression()
+
+# model.fit(X,y)
+
+# result=model.predict([[5]])
+
+# print("prediction", result)
+
+
+
+# Data handle with pandas
+
+# import pandas as pd
+
+# #create data
+
+# data={
+#     "hours": [1,2,3,4,5],
+#     "result":[2,4,6,8,10]
+# }
+
+# df=pd.DataFrame(data)
+# print(df["hours"])
+
+
+
+# simple project with pandas
+
+
+import pandas as pd
+
 from sklearn.linear_model import LinearRegression
 
-X=[[10], [20], 30]
-y=[100,200,300]
 
+data = {
+    "hours":[1,2,3,4,5],
+    "result":[2,4,6,8,10]
+}
+
+df=pd.DataFrame(data)
+
+X = df[["hours"]]
+y= df["result"]
 
 model = LinearRegression()
-
 model.fit(X,y)
 
-result=model.predict([[5]])
+new_data = pd.DataFrame({
+    "hours": [10]
+})
 
-print("prediction", result)
+prediction = model.predict(new_data)
+
+print("prediction is", prediction)
