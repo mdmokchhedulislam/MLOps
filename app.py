7440e3a865f0e55ec89ce986b4f16e361a4d26c0
mokchhedul.islam@spectrum-bd.com <mokchheduls46@gmail.com>
2026-05-07 16:55:04 +0600

    mlflow simple project

--- app.py
+++ app.py
@@ -133,20 +133,81 @@
 
 # model serving simple project 
 
-from fastapi import FastAPI
-import joblib
-import pandas as pd
+# from fastapi import FastAPI
+# import joblib
+# import pandas as pd
 
-app = FastAPI()
+# app = FastAPI()
 
-model = joblib.load("model.pkl")
+# model = joblib.load("model.pkl")
 
-@app.get("/")
-def home():
-    return {"message": "ml api is running"}
+# @app.get("/")
+# def home():
+#     return {"message": "ml api is running"}
 
-@app.get("/predict")
-def predict(hours: int):
-    data = pd.DataFrame({"hours": [hours]})
-    prediction = model.predict(data)
-    return {"prediction": prediction.tolist()}
+# @app.get("/predict")
+# def predict(hours: int):
+#     data = pd.DataFrame({"hours": [hours]})
+#     prediction = model.predict(data)
+#     return {"prediction": prediction.tolist()}
+
+
+# mlflow
+import mlflow
+import mlflow.sklearn
+
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# MLflow DB location
+mlflow.set_tracking_uri("sqlite:///C:/mlflow/mlflow.db")
+
+# experiment name
+mlflow.set_experiment("wine-classification")
+
+# dataset
+wine = load_wine()
+
+# print("wine data is", wine)
+
+X = wine.data
+y = wine.target
+
+# split
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.10,
+    random_state=42
+)
+
+# params
+max_depth = 5
+n_estimators = 10
+
+# start run
+with mlflow.start_run():
+
+    rf = RandomForestClassifier(
+        max_depth=max_depth,
+        n_estimators=n_estimators,
+        random_state=42
+    )
+
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+#     # logging
+    mlflow.log_metric("accuracy", accuracy)
+
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("n_estimators", n_estimators)
+
+#     # save model
+    mlflow.sklearn.log_model(rf, "random_forest_model")
+    print("Accuracy:", accuracy)
